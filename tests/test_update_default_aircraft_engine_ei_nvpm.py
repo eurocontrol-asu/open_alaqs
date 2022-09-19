96e47e333f515b61715851229f0cb97be9a6093d
martyna <martyna@aerlabs.nl>
2022-09-19 15:46:07 +0200

    Clean up test file for update_default_aircraft_engine_ei_nvpm

--- tests/test_update_default_aircraft_engine_ei_nvpm.py
+++ tests/test_update_default_aircraft_engine_ei_nvpm.py
@@ -2,19 +2,28 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
-from open_alaqs.db_updates.update_default_aircraft_engine_ei_nvpm import \
-    SCALING_FACTORS, AIR_FUEL_RATIO
-from open_alaqs.db_updates.update_default_aircraft_engine_ei_nvpm import \
-    calculate_smoke_number, calculate_nvpm_mass_concentration_ck, \
-    calculate_exhaust_volume_qk, calculate_nvpm_mass_ei, \
-    calculate_nvpm_number_ei, calculate_loss_correction_factor, evalute_beta
+from open_alaqs.db_updates.update_default_aircraft_engine_ei_nvpm import (
+    SCALING_FACTORS,
+    AIR_FUEL_RATIO,
+)
+from open_alaqs.db_updates.update_default_aircraft_engine_ei_nvpm import (
+    calculate_smoke_number,
+    calculate_nvpm_mass_concentration_ck,
+    calculate_exhaust_volume_qk,
+    calculate_nvpm_mass_ei,
+    calculate_nvpm_number_ei,
+    calculate_loss_correction_factor,
+    evalute_beta,
+)
 
 RESULT_TOLERANCE = 0.15
 THEORETICAL_VALUES = {
-    "TX": [181.0, 9.2*10**15],
-    "TO": [207.0, 1.3*10**15],
-    "CL": [212.0, 1.3*10**15],
-    "AP": [142.0, 7.2*10**15]}
+    "TX": [181.0, 9.2 * 10**15],
+    "TO": [207.0, 1.3 * 10**15],
+    "CL": [212.0, 1.3 * 10**15],
+    "AP": [142.0, 7.2 * 10**15],
+}
+
 
 @pytest.fixture
 def sample_data():
@@ -27,11 +36,14 @@ def sample_data():
 
     return pd.read_csv(data_path)
 
-def apply_lower_limit(theoretical_value:float)->float:
-    return theoretical_value*(1-RESULT_TOLERANCE)
 
-def apply_upper_limit(theoretical_value:float)->float:
-    return theoretical_value*(1+RESULT_TOLERANCE)
+def apply_lower_limit(theoretical_value: float) -> float:
+    return theoretical_value * (1 - RESULT_TOLERANCE)
+
+
+def apply_upper_limit(theoretical_value: float) -> float:
+    return theoretical_value * (1 + RESULT_TOLERANCE)
+
 
 def test_calculate_nvpm_mass_and_number_tx(sample_data: pd.DataFrame):
     """
@@ -52,8 +64,7 @@ def test_calculate_nvpm_mass_and_number_tx(sample_data: pd.DataFrame):
 
     smoke_number_k = calculate_smoke_number(old_line, engine_scaling_factor)
 
-    nvpm_mass_concentration_ck = calculate_nvpm_mass_concentration_ck(
-        smoke_number_k)
+    nvpm_mass_concentration_ck = calculate_nvpm_mass_concentration_ck(smoke_number_k)
 
     # Evaluate beta
     beta = evalute_beta(old_line)
@@ -64,12 +75,14 @@ def test_calculate_nvpm_mass_and_number_tx(sample_data: pd.DataFrame):
     exhaust_volume_qk = calculate_exhaust_volume_qk(engine_afr, beta)
 
     # Calculate EInvPMmass
-    ei_nvpm_mass_k = calculate_nvpm_mass_ei(nvpm_mass_concentration_ck,
-                                            exhaust_volume_qk)
+    ei_nvpm_mass_k = calculate_nvpm_mass_ei(
+        nvpm_mass_concentration_ck, exhaust_volume_qk
+    )
 
     # Calculate loss correction factor
     loss_correction_factor_kslm_k = calculate_loss_correction_factor(
-        nvpm_mass_concentration_ck, beta)
+        nvpm_mass_concentration_ck, beta
+    )
 
     # Calculate the nvPMmass EIs for each engine mode at the engine exit
     ei_nvpm_mass_ek = loss_correction_factor_kslm_k * ei_nvpm_mass_k
@@ -77,18 +90,19 @@ def test_calculate_nvpm_mass_and_number_tx(sample_data: pd.DataFrame):
     # Calculate EInvPM number (#/kg fuel)
     ei_nvpm_number_ek = calculate_nvpm_number_ei(ei_nvpm_mass_ek, old_line)
 
-
-#TODO: DELETE BELOW 
-    # # In ICAO doc.9889 Section 5, value: 181
-    # assert round(ei_nvpm_mass_ek, 1) == 174.81536
-
-    # # In ICAO doc.9889 Section 5, value: 9.2*10^15 
-    # assert round(ei_nvpm_number_ek, 1) == 8816272110116584.0
-
     theoretical_values = THEORETICAL_VALUES[mode]
 
-    assert (apply_lower_limit(theoretical_values[0]) <= ei_nvpm_mass_ek <= apply_upper_limit(theoretical_values[0]))
-    assert (apply_lower_limit(theoretical_values[1]) <= ei_nvpm_number_ek <= apply_upper_limit(theoretical_values[1]))
+    assert (
+        apply_lower_limit(theoretical_values[0])
+        <= ei_nvpm_mass_ek
+        <= apply_upper_limit(theoretical_values[0])
+    )
+    assert (
+        apply_lower_limit(theoretical_values[1])
+        <= ei_nvpm_number_ek
+        <= apply_upper_limit(theoretical_values[1])
+    )
+
 
 def test_calculate_nvpm_mass_and_number_to(sample_data: pd.DataFrame):
     """
@@ -108,8 +122,7 @@ def test_calculate_nvpm_mass_and_number_to(sample_data: pd.DataFrame):
 
     smoke_number_k = calculate_smoke_number(old_line, engine_scaling_factor)
 
-    nvpm_mass_concentration_ck = calculate_nvpm_mass_concentration_ck(
-        smoke_number_k)
+    nvpm_mass_concentration_ck = calculate_nvpm_mass_concentration_ck(smoke_number_k)
 
     # Evaluate beta
     beta = evalute_beta(old_line)
@@ -120,12 +133,14 @@ def test_calculate_nvpm_mass_and_number_to(sample_data: pd.DataFrame):
     exhaust_volume_qk = calculate_exhaust_volume_qk(engine_afr, beta)
 
     # Calculate EInvPMmass
-    ei_nvpm_mass_k = calculate_nvpm_mass_ei(nvpm_mass_concentration_ck,
-                                            exhaust_volume_qk)
+    ei_nvpm_mass_k = calculate_nvpm_mass_ei(
+        nvpm_mass_concentration_ck, exhaust_volume_qk
+    )
 
     # Calculate loss correction factor
     loss_correction_factor_kslm_k = calculate_loss_correction_factor(
-        nvpm_mass_concentration_ck, beta)
+        nvpm_mass_concentration_ck, beta
+    )
 
     # Calculate the nvPMmass EIs for each engine mode at the engine exit
     ei_nvpm_mass_ek = loss_correction_factor_kslm_k * ei_nvpm_mass_k
@@ -133,17 +148,19 @@ def test_calculate_nvpm_mass_and_number_to(sample_data: pd.DataFrame):
     # Calculate EInvPM number (#/kg fuel)
     ei_nvpm_number_ek = calculate_nvpm_number_ei(ei_nvpm_mass_ek, old_line)
 
-    # # In ICAO doc.9889 Section 5, value: 207 
-    # assert round(ei_nvpm_mass_ek, 5) == 202.37401 
-
-    # # In ICAO value 1.3*10^15
-    # assert round(ei_nvpm_number_ek, 5) == 1275763472297245.2
-
-    
     theoretical_values = THEORETICAL_VALUES[mode]
 
-    assert (apply_lower_limit(theoretical_values[0]) <= ei_nvpm_mass_ek <= apply_upper_limit(theoretical_values[0]))
-    assert (apply_lower_limit(theoretical_values[1]) <= ei_nvpm_number_ek <= apply_upper_limit(theoretical_values[1]))
+    assert (
+        apply_lower_limit(theoretical_values[0])
+        <= ei_nvpm_mass_ek
+        <= apply_upper_limit(theoretical_values[0])
+    )
+    assert (
+        apply_lower_limit(theoretical_values[1])
+        <= ei_nvpm_number_ek
+        <= apply_upper_limit(theoretical_values[1])
+    )
+
 
 def test_calculate_nvpm_mass_and_number_ap(sample_data: pd.DataFrame):
     """
@@ -162,8 +179,7 @@ def test_calculate_nvpm_mass_and_number_ap(sample_data: pd.DataFrame):
 
     smoke_number_k = calculate_smoke_number(old_line, engine_scaling_factor)
 
-    nvpm_mass_concentration_ck = calculate_nvpm_mass_concentration_ck(
-        smoke_number_k)
+    nvpm_mass_concentration_ck = calculate_nvpm_mass_concentration_ck(smoke_number_k)
 
     # Evaluate beta
     beta = evalute_beta(old_line)
@@ -174,12 +190,14 @@ def test_calculate_nvpm_mass_and_number_ap(sample_data: pd.DataFrame):
     exhaust_volume_qk = calculate_exhaust_volume_qk(engine_afr, beta)
 
     # Calculate EInvPMmass
-    ei_nvpm_mass_k = calculate_nvpm_mass_ei(nvpm_mass_concentration_ck,
-                                            exhaust_volume_qk)
+    ei_nvpm_mass_k = calculate_nvpm_mass_ei(
+        nvpm_mass_concentration_ck, exhaust_volume_qk
+    )
 
     # Calculate loss correction factor
     loss_correction_factor_kslm_k = calculate_loss_correction_factor(
-        nvpm_mass_concentration_ck, beta)
+        nvpm_mass_concentration_ck, beta
+    )
 
     # Calculate the nvPMmass EIs for each engine mode at the engine exit
     ei_nvpm_mass_ek = loss_correction_factor_kslm_k * ei_nvpm_mass_k
@@ -187,17 +205,18 @@ def test_calculate_nvpm_mass_and_number_ap(sample_data: pd.DataFrame):
     # Calculate EInvPM number (#/kg fuel)
     ei_nvpm_number_ek = calculate_nvpm_number_ei(ei_nvpm_mass_ek, old_line)
 
-    # # In ICAO doc.9889 Section 5, value: 142
-    # assert round(ei_nvpm_mass_ek, 5) == 137.01267
-
-    # # In ICAO value 7.2*10^15
-    # assert round(ei_nvpm_number_ek, 5) == 6909810707895345.0
-
-
     theoretical_values = THEORETICAL_VALUES[mode]
 
-    assert (apply_lower_limit(theoretical_values[0]) <= ei_nvpm_mass_ek <= apply_upper_limit(theoretical_values[0]))
-    assert (apply_lower_limit(theoretical_values[1]) <= ei_nvpm_number_ek <= apply_upper_limit(theoretical_values[1]))
+    assert (
+        apply_lower_limit(theoretical_values[0])
+        <= ei_nvpm_mass_ek
+        <= apply_upper_limit(theoretical_values[0])
+    )
+    assert (
+        apply_lower_limit(theoretical_values[1])
+        <= ei_nvpm_number_ek
+        <= apply_upper_limit(theoretical_values[1])
+    )
 
 
 def test_calculate_nvpm_mass_and_number_cl(sample_data: pd.DataFrame):
@@ -217,8 +236,7 @@ def test_calculate_nvpm_mass_and_number_cl(sample_data: pd.DataFrame):
 
     smoke_number_k = calculate_smoke_number(old_line, engine_scaling_factor)
 
-    nvpm_mass_concentration_ck = calculate_nvpm_mass_concentration_ck(
-        smoke_number_k)
+    nvpm_mass_concentration_ck = calculate_nvpm_mass_concentration_ck(smoke_number_k)
 
     # Evaluate beta
     beta = evalute_beta(old_line)
@@ -229,12 +247,14 @@ def test_calculate_nvpm_mass_and_number_cl(sample_data: pd.DataFrame):
     exhaust_volume_qk = calculate_exhaust_volume_qk(engine_afr, beta)
 
     # Calculate EInvPMmass
-    ei_nvpm_mass_k = calculate_nvpm_mass_ei(nvpm_mass_concentration_ck,
-                                            exhaust_volume_qk)
+    ei_nvpm_mass_k = calculate_nvpm_mass_ei(
+        nvpm_mass_concentration_ck, exhaust_volume_qk
+    )
 
     # Calculate loss correction factor
     loss_correction_factor_kslm_k = calculate_loss_correction_factor(
-        nvpm_mass_concentration_ck, beta)
+        nvpm_mass_concentration_ck, beta
+    )
 
     # Calculate the nvPMmass EIs for each engine mode at the engine exit
     ei_nvpm_mass_ek = loss_correction_factor_kslm_k * ei_nvpm_mass_k
@@ -242,12 +262,15 @@ def test_calculate_nvpm_mass_and_number_cl(sample_data: pd.DataFrame):
     # Calculate EInvPM number (#/kg fuel)
     ei_nvpm_number_ek = calculate_nvpm_number_ei(ei_nvpm_mass_ek, old_line)
 
-    # # In ICAO doc.9889 Section 5, value: 212
-    # assert round(ei_nvpm_mass_ek, 5) == 208.71973
-    # # In ICAO value 1.3*10^15
-    # assert round(ei_nvpm_number_ek, 5) == 1315766806745960.5
-
     theoretical_values = THEORETICAL_VALUES[mode]
 
-    assert (apply_lower_limit(theoretical_values[0]) <= ei_nvpm_mass_ek <= apply_upper_limit(theoretical_values[0]))
-    assert (apply_lower_limit(theoretical_values[1]) <= ei_nvpm_number_ek <= apply_upper_limit(theoretical_values[1]))
+    assert (
+        apply_lower_limit(theoretical_values[0])
+        <= ei_nvpm_mass_ek
+        <= apply_upper_limit(theoretical_values[0])
+    )
+    assert (
+        apply_lower_limit(theoretical_values[1])
+        <= ei_nvpm_number_ek
+        <= apply_upper_limit(theoretical_values[1])
+    )
