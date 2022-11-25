38e7b079fe516b8811ed98f8990483c411f2746b
Robert Koster <robert@aerlabs.com>
2022-11-25 13:47:22 +0100

    Refactor code. Add tests for generating templates.
    Related to #199, #207, #208

--- database/generate_templates.py
+++ database/generate_templates.py
@@ -13,6 +13,12 @@ SQL_DIR = Path(__file__).parent / 'sql'
 DATA_DIR = Path(__file__).parent / 'data'
 TEMPLATES_DIR = Path(__file__).parents[1] / 'alaqs_core/templates'
 
+# Set the match patterns
+MATCH_PATTERNS = {
+    'project': r'(default|user)_(.*).sql',
+    'inventory': r'(default|user|tbl)_(.*).sql'
+}
+
 
 def get_engine(p: Path) -> sqlalchemy.engine.Engine:
     if isinstance(p, PosixPath):
@@ -63,6 +69,29 @@ def get_data(p: Path, d: pd.DataFrame):
     return data
 
 
+def apply_sql(engine, sql_paths, file_type):
+    if file_type not in ('project', 'inventory'):
+        raise ValueError(f'{file_type} is not supported. It should be either \'project\' or \'inventory\'')
+
+    # Determine the match pattern
+    match_pattern = MATCH_PATTERNS[file_type]
+
+    for sql_path in sql_paths:
+
+        # Read the .sql file
+        with sql_path.open() as file:
+            sql = file.read()
+
+        # Check if the SQL query should be executed to the file
+        if re.search(match_pattern, sql_path.name) is not None:
+
+            # Execute the SQL query
+            for s in sql.split(';'):
+                if len(s) > 0 and ("AddGeometryColumn" not in s) and (
+                        file_type != 'inventory' or not s.strip().startswith("INSERT INTO ")):
+                    engine.execute(f"{s};")
+
+
 if __name__ == "__main__":
     """
     Build a new *.alaqs project template and a new *_out.alaqs inventory template. 
@@ -84,30 +113,12 @@ if __name__ == "__main__":
     shutil.copy(editable_layers_template_path, project_template)
     shutil.copy(editable_layers_template_path, inventory_template)
 
-    # Get the SQL queries to execute
-    for sql_path in SQL_DIR.glob('*.sql'):
+    # Get the files containing SQL queries
+    sql_files = list(SQL_DIR.glob('*.sql'))
 
-        print('create', sql_path.stem)
-
-        # Read the .sql file
-        with sql_path.open() as file:
-            sql = file.read()
-
-        # Check if the SQL query should be executed to the project template
-        if re.search(r'(default|user)_(.*).sql', sql_path.name) is not None:
-
-            # Execute the SQL query
-            for s in sql.split(';'):
-                if len(s) > 0 and ("AddGeometryColumn" not in s):
-                    project_engine.execute(f"{s};")
-
-        # Check if the SQL query should be executed to the inventory template
-        if re.search(r'(default|user|tbl)_(.*).sql', sql_path.name) is not None:
-
-            # Execute the SQL query
-            for s in sql.split(';'):
-                if len(s) > 0 and ("AddGeometryColumn" not in s) and not s.strip().startswith("INSERT INTO "):
-                    inventory_engine.execute(f"{s};")
+    # Execute the SQL queries in the files to the templates
+    apply_sql(project_engine, sql_files, file_type='project')
+    apply_sql(inventory_engine, sql_files, file_type='inventory')
 
     # Get the csv files to import
     for csv_path in DATA_DIR.glob('*.csv'):
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,53 +1,84 @@
 import re
+import unittest
 from pathlib import Path
+from warnings import warn
 
+import pandas as pd
+import pytest
 from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
 
-SQL_DIR = Path(__file__).parents[1] / 'database/sql'
+from database.generate_templates import get_data, apply_sql, MATCH_PATTERNS
+
+DB_DIR = Path(__file__).parents[1] / 'database'
+SQL_DIR = DB_DIR / 'sql'
+DATA_DIR = DB_DIR / 'data'
 
 
-def test_sql():
+@pytest.fixture(scope="module")
+def sql_files():
+    # Get the files with SQL queries to execute
+    return list(SQL_DIR.glob('*.sql'))
+
+
+@pytest.fixture(scope="module")
+def csv_files():
+    # Get the files with SQL queries to execute
+    return list(DATA_DIR.glob('*.csv'))
+
+
+@pytest.mark.parametrize("template_type", ['project', 'inventory'])
+def test_sql(sql_files: list, template_type: str):
     """
     Test if the sql files can be processed
     """
 
-    # Create in-memory sqlite databases
-    project_engine = create_engine("sqlite:///:memory:")
-    inventory_engine = create_engine("sqlite:///:memory:")
+    # Create in-memory sqlite database
+    engine = create_engine("sqlite:///:memory:")
+
+    # Execute the SQL queries in the files to the templates
+    apply_sql(engine, sql_files, file_type=template_type)
 
     # Get the SQL queries to execute
-    for sql_path in SQL_DIR.glob('*.sql'):
-
-        print('create', sql_path.stem)
-
-        # Read the .sql file
-        with sql_path.open() as file:
-            sql = file.read()
+    for sql_path in sql_files:
 
         # Check if the SQL query should be executed to the project template
-        if re.search(r'(default|user)_(.*).sql', sql_path.name) is not None:
-
-            # Execute the SQL query
-            for s in sql.split(';'):
-                if len(s) > 0 and ("AddGeometryColumn" not in s):
-                    project_engine.execute(f"{s};")
-
-            assert sql_path.stem in project_engine.table_names()
-
-        # Check if the SQL query should be executed to the inventory template
-        if re.search(r'(default|user|tbl)_(.*).sql', sql_path.name) is not None:
-
-            # Execute the SQL query
-            for s in sql.split(';'):
-                if len(s) > 0 and ("AddGeometryColumn" not in s) and not s.strip().startswith("INSERT INTO "):
-                    inventory_engine.execute(f"{s};")
-
-            assert sql_path.stem in inventory_engine.table_names()
+        if re.search(MATCH_PATTERNS[template_type], sql_path.name) is not None:
+            # Check if the table is present
+            assert sql_path.stem in engine.table_names()
 
 
-def test_csv():
-    # todo Test if the csv files can be processed
-    raise NotImplementedError
+@pytest.mark.parametrize("template_type", ['project', 'inventory'])
+def test_csv(sql_files: list, csv_files: list, template_type: str):
+    """
+    Test if the csv files can be processed
+    """
+
+    # Create in-memory sqlite database
+    engine = create_engine("sqlite:///:memory:")
+
+    # Execute the SQL queries in the files to the templates
+    apply_sql(engine, sql_files, file_type=template_type)
+
+    # Get the csv files to import
+    for csv_path in csv_files:
+
+        print('import', csv_path.stem)
+
+        # Get the contents of the table
+        project_data = pd.read_sql(f"SELECT * FROM {csv_path.stem}", engine)
+
+        # Read the .csv file (try ';' and ',' as separators)
+        data = get_data(csv_path, project_data)
+
+        # Import the data to fill the table
+        if data is not None and project_data.empty and not data.empty:
+            try:
+                data.to_sql(csv_path.stem, engine, index=False, if_exists='append')
+            except IntegrityError as e:
+                warn(e.args[0])
+        elif not project_data.empty:
+            raise ValueError("What to do when the database is not empty?")
 
 
 def test_project_template():
