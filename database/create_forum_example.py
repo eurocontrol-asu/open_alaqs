d0cb4a7220464b77800da5c064ffa5d22d6213ef
Robert Koster <robert@aerlabs.com>
2023-03-02 19:03:25 +0100

    Add input files to Forum example

--- database/create_forum_example.py
+++ database/create_forum_example.py
@@ -24,14 +24,17 @@ if __name__ == "__main__":
     original_database = SRC_DIR / 'N06_E02A_B19_out.alaqs'
 
     # Set the path to the example
-    example_path = EXAMPLES_DIR / 'forum_out.alaqs'
+    example_path = EXAMPLES_DIR / 'forum.alaqs'
+    example_out_path = EXAMPLES_DIR / 'forum_out.alaqs'
+    example_movements_path = EXAMPLES_DIR / 'forum_movements.csv'
+    example_meteo_path = EXAMPLES_DIR / 'forum_meteo.csv'
 
     # Duplicate the QGIS template to act as basis for the new project and inventory templates
     logging.info(f'duplicate {original_database.name}')
-    shutil.copy(original_database, example_path)
+    shutil.copy(original_database, example_out_path)
 
     # Create the sqlite engines to the databases
-    example_engine = get_engine(example_path)
+    example_out_engine = get_engine(example_out_path)
 
     # Set the tables to keep (read: copy from original file)
     to_keep = [
@@ -60,7 +63,7 @@ if __name__ == "__main__":
     sql_files = list(f for f in SQL_DIR.glob('*.sql') if f.stem not in to_keep)
 
     # Execute the SQL queries in the files to the templates
-    apply_sql(example_engine, sql_files, file_type='inventory')
+    apply_sql(example_out_engine, sql_files, file_type='inventory')
 
     # Get the csv files to load
     csv_files = sorted(f for f in DATA_DIR.glob('*.csv') if f.stem not in to_keep)
@@ -71,7 +74,7 @@ if __name__ == "__main__":
         logging.debug(f'import {csv_path.stem}')
 
         # Get the contents of the table
-        project_data = pd.read_sql(f"SELECT * FROM {csv_path.stem}", example_engine)
+        project_data = pd.read_sql(f"SELECT * FROM {csv_path.stem}", example_out_engine)
 
         # Read the .csv file
         data = pd.read_csv(csv_path)
@@ -79,9 +82,34 @@ if __name__ == "__main__":
         # Import the data to fill the table
         if data is not None and project_data.empty and not data.empty:
             try:
-                data.to_sql(csv_path.stem, example_engine, index=False, if_exists='append')
+                data.to_sql(csv_path.stem, example_out_engine, index=False, if_exists='append')
                 logging.debug(f'successfully imported {data.shape[0]} rows')
             except IntegrityError as e:
                 logging.error(e.args[0])
         elif not project_data.empty:
             raise ValueError("What to do when the database is not empty?")
+
+    # Duplicate the QGIS template to act as basis for the new project and inventory templates
+    logging.info(f'duplicate {example_out_path.name}')
+    shutil.copy(example_out_path, example_path)
+
+    # Create the sqlite engines to the databases
+    example_engine = get_engine(example_path)
+
+    # Set the input specific tables and truncate them
+    inventory_tables = [
+        "user_aircraft_movements",
+    ]
+    for inventory_table in inventory_tables:
+        example_engine.execute(f'DELETE FROM "{inventory_table}";')
+
+    # Get the inventory specific tables and drop them
+    for sql_file in SQL_DIR.glob('tbl_*.sql'):
+        inventory_table = sql_file.stem
+        example_engine.execute(f'DROP TABLE IF EXISTS "{inventory_table}";')
+
+    # Check if the files exist
+    assert example_path.exists(), example_path
+    assert example_out_path.exists(), example_out_path
+    assert example_movements_path.exists(), example_movements_path
+    assert example_meteo_path.exists(), example_meteo_path
