4f90dc72f156a09ffd5459e24e559ecf234a09a5
Ivan Ivanov <ivan.ivanov@suricactus.com>
2024-06-03 16:21:49 +0300

    Add `TabularOutputModule` and simplify CSV export

--- open_alaqs/core/modules/CSVOutputModule.py
+++ open_alaqs/core/modules/CSVOutputModule.py
@@ -1,19 +1,15 @@
+import csv
 import os
-from datetime import datetime
-from typing import List, Tuple
 
 from qgis.PyQt import QtWidgets
 
 from open_alaqs.core.alaqslogging import get_logger
-from open_alaqs.core.interfaces.Emissions import Emission
-from open_alaqs.core.interfaces.OutputModule import OutputModule
-from open_alaqs.core.interfaces.Source import Source
-from open_alaqs.core.tools.csv_interface import write_csv
+from open_alaqs.core.modules.TabularOutputModule import TabularOutputModule
 
 logger = get_logger(__name__)
 
 
-class CSVOutputModule(OutputModule):
+class CSVOutputModule(TabularOutputModule):
     """
     Module to handle csv writes of emission-calculation results
     (timestamp, source, total_emissions_)
@@ -27,123 +23,21 @@ class CSVOutputModule(OutputModule):
     def getModuleDisplayName():
         return "CSV"
 
-    settings_schema = {
-        "has_detailed_output": {
-            "label": "Detailed output",
-            "widget_type": QtWidgets.QCheckBox,
-            "initial_value": False,
-        },
-    }
-
-    def __init__(self, values_dict=None):
-        if values_dict is None:
-            values_dict = {}
-        OutputModule.__init__(self, values_dict)
-        self._isDetailedOutput = values_dict.get("has_detailed_output", False)
-
-        self._rows = []
-        self._headers = []
-
-    def beginJob(self):
-        """
-        Initialize the module
-        """
-
-        # Set the header
-        header = [
-            "Time",
-            "Source name",
-            "CO [kg]",
-            "CO2 [kg]",
-            "HC [kg]",
-            "NOx [kg]",
-            "SOx [kg]",
-            "PMTotal [kg]",
-            "PM01 [kg]",
-            "PM25 [kg]",
-            "PMSul [kg]",
-            "PMVolatile [kg]",
-            "PMNonVolatile [kg]",
-            "PMNonVolatileNumber [-]",
-        ]
-
-        # Initialize rows attribute with the header
-        self._rows = [header]
-
-        # Ask the user to the set the output path
-        file_, handler_ = QtWidgets.QFileDialog.getSaveFileName(
-            None, "Save results as CSV file", ".", "CSV (*.csv)"
-        )
-
-        # Set the output path
-        self.setOutputPath(file_)
-
-    def process(
-        self, timeval: datetime, result: List[Tuple[Source, Emission]], **kwargs
-    ):
-        """
-        Process the results and create the records of the csv
-
-        :param timeval:
-        :param result:
-        :param kwargs:
-        """
-        # result is of format [(Source, Emission)]
-
-        if not self._isDetailedOutput:
-            # Sum all emissions
-            total_emissions_ = sum(
-                [sum(emissions_) for (_, emissions_) in result if emissions_]
-            )
-
-            self._rows.append(
-                [
-                    timeval,
-                    "total",
-                    total_emissions_.getCO(unit="kg")[0],
-                    total_emissions_.getCO2(unit="kg")[0],
-                    total_emissions_.getHC(unit="kg")[0],
-                    total_emissions_.getNOx(unit="kg")[0],
-                    total_emissions_.getSOx(unit="kg")[0],
-                    total_emissions_.getPM10(unit="kg")[0],
-                    total_emissions_.getPM1(unit="kg")[0],
-                    total_emissions_.getPM2(unit="kg")[0],
-                    total_emissions_.getPM10Sul(unit="kg")[0],
-                    total_emissions_.getPM10Organic(unit="kg")[0],
-                    total_emissions_.getnvPM(unit="kg")[0],
-                    total_emissions_.getnvPMnumber()[0],
-                ]
-            )
-        else:
-            for source, emissions_ in result:
-                self._rows.append(
-                    [
-                        timeval,
-                        # emissions.getCategory()
-                        source.getName(),
-                        sum(emissions_).getCO(unit="kg")[0],
-                        sum(emissions_).getCO2(unit="kg")[0],
-                        sum(emissions_).getHC(unit="kg")[0],
-                        sum(emissions_).getNOx(unit="kg")[0],
-                        sum(emissions_).getSOx(unit="kg")[0],
-                        sum(emissions_).getPM10(unit="kg")[0],
-                        sum(emissions_).getPM1(unit="kg")[0],
-                        sum(emissions_).getPM2(unit="kg")[0],
-                        sum(emissions_).getPM10Sul(unit="kg")[0],
-                        sum(emissions_).getPM10Organic(unit="kg")[0],
-                        sum(emissions_).getnvPM(unit="kg")[0],
-                        sum(emissions_).getnvPMnumber()[0],
-                    ]
-                )
-
     def endJob(self):
         """
         Write output to csv file
         """
-        filename = self.getOutputPath()
+        filename, handler_ = QtWidgets.QFileDialog.getSaveFileName(
+            None, "Save results as CSV file", ".", "CSV (*.csv)"
+        )
 
-        if filename:
-            write_csv(filename, self._rows)
+        if not filename:
+            return
+
+        with open(filename, "w") as f:
+            writer = csv.DictWriter(f, list(self.fields.keys()))
+            writer.writeheader()
+            writer.writerows(self.rows)
 
         if os.path.isfile(filename):
             QtWidgets.QMessageBox.information(
