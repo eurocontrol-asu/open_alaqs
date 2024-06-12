3f80101c3b06d128c3311ae0152b25cffe7ba285
Ivan Ivanov <ivan.ivanov@suricactus.com>
2024-06-12 04:36:41 +0300

    Retire `TabularOutputModule`

--- open_alaqs/core/modules/TableViewWidgetOutputModule.py
+++ open_alaqs/core/modules/TableViewWidgetOutputModule.py
@@ -1,5 +1,6 @@
 import csv
 import os
+from datetime import datetime
 from typing import Any, Optional
 
 from qgis.PyQt import QtWidgets
@@ -7,8 +8,10 @@ from qgis.PyQt.QtWidgets import QTableWidgetItem
 from qgis.PyQt.uic import loadUiType
 
 from open_alaqs.core.alaqslogging import get_logger
+from open_alaqs.core.interfaces.Emissions import Emission
+from open_alaqs.core.interfaces.OutputModule import OutputModule
+from open_alaqs.core.interfaces.Source import Source
 from open_alaqs.core.interfaces.SQLSerializable import SQLSerializable
-from open_alaqs.core.modules.TabularOutputModule import TabularOutputModule
 from open_alaqs.core.tools.sql_interface import insert_into_table
 
 Ui_TableViewDialog, _ = loadUiType(
@@ -18,9 +21,9 @@ Ui_TableViewDialog, _ = loadUiType(
 logger = get_logger(__name__)
 
 
-class TableViewWidgetOutputModule(TabularOutputModule):
+class TableViewWidgetOutputModule(OutputModule):
     """
-    Module to plot results of emission calculation in a table
+    Module to plot results of emission calculation in a table and export the results to CSV or SQLite
     """
 
     table_fields = [
@@ -39,6 +42,33 @@ class TableViewWidgetOutputModule(TabularOutputModule):
         "pmnonvolatile_number",
     ]
 
+    settings_schema = {
+        "has_detailed_output": {
+            "label": "Detailed output",
+            "widget_type": QtWidgets.QCheckBox,
+            "initial_value": False,
+        },
+    }
+
+    fields = {
+        "timestamp": "Timestamp",
+        "source_type": "Source Type",
+        "source_name": "Source Name",
+        "co_kg": "CO [kg]",
+        "co2_kg": "CO2 [kg]",
+        "hc_kg": "HC [kg]",
+        "nox_kg": "NOX [kg]",
+        "sox_kg": "SOX [kg]",
+        "pmtotal_kg": "PMTotal [kg]",
+        "pm01_kg": "PM01 [kg]",
+        "pm25_kg": "PM25 [kg]",
+        "pmsul_kg": "PMSUL [kg]",
+        "pmvolatile_kg": "PMVolatile [kg]",
+        "pmnonvolatile_kg": "PMNonVolatile [kg]",
+        "pmnonvolatile_number": "PMNonVolatileNumber [er]",
+        "source_wkt": "Source WKT",
+    }
+
     @staticmethod
     def getModuleName():
         return "TableViewWidgetOutputModule"
@@ -50,17 +80,45 @@ class TableViewWidgetOutputModule(TabularOutputModule):
     def __init__(self, values_dict: dict[str, Any]) -> None:
         super().__init__(values_dict)
 
-        self.start_dt = values_dict["start_dt_inclusive"]
-        self.end_dt = values_dict["end_dt_inclusive"]
+        self._start_dt = values_dict["start_dt_inclusive"]
+        self._end_dt = values_dict["end_dt_inclusive"]
+        self._has_detailed_output = values_dict["has_detailed_output"]
 
+        # Output rows
+        self.rows: list[dict[str, Any]] = []
+
+        # Output UI
         self.widget = EmissionsTableViewDialog(values_dict["parent"])
-
         self.widget.ui.exportCsvBtn.clicked.connect(
             lambda: self._on_export_csv_clicked()
         )
         self.widget.ui.exportSqliteBtn.clicked.connect(
             lambda: self._on_export_sqlite_clicked()
         )
+
+    def process(
+        self,
+        timestamp: datetime,
+        result: list[tuple[Source, Emission]],
+        **kwargs: Any,
+    ) -> None:
+        """
+        Process the results and create the records of the csv
+        """
+        if self._start_dt and self._end_dt:
+            if not (self._start_dt <= timestamp < self._end_dt):
+                return None
+
+        if self._has_detailed_output:
+            for source, emissions in result:
+                emissions_sum = sum(emissions)
+                self.rows.append(self._format_row(timestamp, emissions_sum, source))
+        else:
+            emissions_total = sum(
+                [sum(emissions_) for (_, emissions_) in result if emissions_]
+            )
+
+            self.rows.append(self._format_row(timestamp, emissions_total, None))
 
     def endJob(self) -> QtWidgets.QDialog:
         headers = [self.fields[k] for k in self.table_fields]
@@ -84,6 +142,44 @@ class TableViewWidgetOutputModule(TabularOutputModule):
             self.widget.add_row(formatted_row)
 
         return self.widget
+
+    def _format_row(
+        self,
+        timestamp: datetime,
+        emissions: Emission,
+        source: Optional[Source],
+    ) -> dict[str, Any]:
+        if source is None:
+            source_type = "total"
+            source_name = "total"
+            source_wkt = None
+        else:
+            source_type = source.__class__.__name__
+            source_name = source.getName()
+
+            if hasattr(source, "getGeometryText"):
+                source_wkt = source.getGeometryText()
+            else:
+                source_wkt = None
+
+        return {
+            "timestamp": timestamp.isoformat(),
+            "source_wkt": source_wkt,
+            "source_type": source_type,
+            "source_name": source_name,
+            "co_kg": emissions.getCO(unit="kg")[0],
+            "co2_kg": emissions.getCO2(unit="kg")[0],
+            "hc_kg": emissions.getHC(unit="kg")[0],
+            "nox_kg": emissions.getNOx(unit="kg")[0],
+            "sox_kg": emissions.getSOx(unit="kg")[0],
+            "pmtotal_kg": emissions.getPM10(unit="kg")[0],
+            "pm01_kg": emissions.getPM1(unit="kg")[0],
+            "pm25_kg": emissions.getPM2(unit="kg")[0],
+            "pmsul_kg": emissions.getPM10Sul(unit="kg")[0],
+            "pmvolatile_kg": emissions.getPM10Organic(unit="kg")[0],
+            "pmnonvolatile_kg": emissions.getnvPM(unit="kg")[0],
+            "pmnonvolatile_number": emissions.getnvPMnumber()[0],
+        }
 
     def _on_export_csv_clicked(self):
         filename, handler_ = QtWidgets.QFileDialog.getSaveFileName(
