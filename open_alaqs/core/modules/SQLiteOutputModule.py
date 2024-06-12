81fbd7ad220306c9bfdfcc4bb8de6ddc0657470c
Ivan Ivanov <ivan.ivanov@suricactus.com>
2024-06-12 04:14:52 +0300

    Inherit `TabularOutputModule` in `SQLiteOutputModule`

--- open_alaqs/core/modules/SQLiteOutputModule.py
+++ open_alaqs/core/modules/SQLiteOutputModule.py
@@ -1,29 +1,20 @@
-from collections import OrderedDict
+import os
 
 from qgis.PyQt import QtWidgets
 
 from open_alaqs.core.alaqslogging import get_logger
-from open_alaqs.core.interfaces.OutputModule import OutputModule
 from open_alaqs.core.interfaces.SQLSerializable import SQLSerializable
-from open_alaqs.core.tools.Singleton import Singleton
+from open_alaqs.core.modules.TabularOutputModule import TabularOutputModule
 from open_alaqs.core.tools.sql_interface import insert_into_table
 
 logger = get_logger(__name__)
 
 
-class SQLiteOutputModule(OutputModule):
+class SQLiteOutputModule(TabularOutputModule):
     """
     Module to handle sql writeout of emission-calculation results (timestamp,
-     source, total_emissions_)
+    source, total_emissions_)
     """
-
-    settings_schema = {
-        "has_detailed_output": {
-            "label": "Detailed output",
-            "widget_type": QtWidgets.QCheckBox,
-            "initial_value": False,
-        },
-    }
 
     @staticmethod
     def getModuleName():
@@ -33,128 +24,55 @@ class SQLiteOutputModule(OutputModule):
     def getModuleDisplayName():
         return "SQLite"
 
-    def __init__(self, values_dict=None):
-        if values_dict is None:
-            values_dict = {}
-        OutputModule.__init__(self, values_dict)
-        self._isDetailedOutput = values_dict.get("has_detailed_output", False)
-
-    def beginJob(self):
-        # initialize database connections
-        self._db = None
-        self.setOutputPath(
-            QtWidgets.QFileDialog.getSaveFileName(
-                None, "Save results as SQLite file", ".", "'SQLite (*.db)'"
-            )
-        )
-
-        filename = self.getOutputPath()[0]
-        if filename:
-            self._db = EmissionCalculationResultDatabase(filename)
-            self._db._recreate_table(filename)
-        else:
-            QtWidgets.QMessageBox.warning(
-                None, "SQLiteOutputModule", "No export DB filename specified"
-            )
-
-    def process(self, timeval, result, **kwargs):
-        # result is of format [(Source, Emission)]
-
-        rows_ = []
-        if not self._isDetailedOutput:
-            total_emissions_ = sum([sum(emissions_) for (source, emissions_) in result])
-            rows_.append(
-                {
-                    "time": timeval,
-                    "source_id": "total",
-                    "co_kg": total_emissions_.getCO(unit="kg")[0],
-                    "co2_kg": total_emissions_.getCO2(unit="kg")[0],
-                    "hc_kg": total_emissions_.getHC(unit="kg")[0],
-                    "nox_kg": total_emissions_.getNOx(unit="kg")[0],
-                    "sox_kg": total_emissions_.getSOx(unit="kg")[0],
-                    "pmtotal_kg": total_emissions_.getPM10(unit="kg")[0],
-                    "pm01_kg": total_emissions_.getPM1(unit="kg")[0],
-                    "pm25_kg": total_emissions_.getPM2(unit="kg")[0],
-                    "pmsul_kg": total_emissions_.getPM10Sul(unit="kg")[0],
-                    "pmvolatile_kg": total_emissions_.getPM10Organic(unit="kg")[0],
-                    "pmnonvolatile_kg": total_emissions_.getnvPM(unit="kg")[0],
-                    "pmnonvolatile_number": total_emissions_.getnvPMnumber()[0],
-                }
-            )
-        else:
-            for source, emissions_ in result:
-                rows_.append(
-                    {
-                        "time": timeval,
-                        "source_id": source.getName(),
-                        "co_kg": sum(emissions_).getCO(unit="kg")[0],
-                        "co2_kg": sum(emissions_).getCO2(unit="kg")[0],
-                        "hc_kg": sum(emissions_).getHC(unit="kg")[0],
-                        "nox_kg": sum(emissions_).getNOx(unit="kg")[0],
-                        "sox_kg": sum(emissions_).getSOx(unit="kg")[0],
-                        "pmtotal_kg": sum(emissions_).getPM10(unit="kg")[0],
-                        "pm01_kg": sum(emissions_).getPM1(unit="kg")[0],
-                        "pm25_kg": sum(emissions_).getPM2(unit="kg")[0],
-                        "pmsul_kg": sum(emissions_).getPM10Sul(unit="kg")[0],
-                        "pmvolatile_kg": sum(emissions_).getPM10Organic(unit="kg")[0],
-                        "pmnonvolatile_kg": sum(emissions_).getnvPM(unit="kg")[0],
-                        "pmnonvolatile_number": sum(emissions_).getnvPMnumber()[0],
-                    }
-                )
-
-        if rows_ and self._db is not None:
-            insert_into_table(self.getOutputPath()[0], self._db._table_name, rows_)
-
     def endJob(self):
-        QtWidgets.QMessageBox.information(
-            None,
-            "SQLiteOutputModule",
-            f"Results saved as SQLite file at `{self.getOutputPath()[0]}`",
+        # initialize database connections
+        filename, handler_ = QtWidgets.QFileDialog.getSaveFileName(
+            None, "Save results as SQLite file", ".", "'SQLite (*.db)'"
         )
 
+        if not filename:
+            return
 
-class EmissionCalculationResultDatabase(SQLSerializable, metaclass=Singleton):
-    """
-    Class that grants access to table in the spatialite database where results
-     can be stored in
-    """
+        table_name = "emission_calculation_result"
+        serializer = SQLSerializable(
+            filename,
+            table_name,
+            {
+                "timestamp": "DATETIME",
+                "source_type": "TEXT",
+                "source_name": "TEXT",
+                "co_kg": "DECIMAL",
+                "co2_kg": "DECIMAL",
+                "hc_kg": "DECIMAL",
+                "nox_kg": "DECIMAL",
+                "sox_kg": "DECIMAL",
+                "pmtotal_kg": "DECIMAL",
+                "pm01_kg": "DECIMAL",
+                "pm25_kg": "DECIMAL",
+                "pmsul_kg": "DECIMAL",
+                "pmvolatile_kg": "DECIMAL",
+                "pmnonvolatile_kg": "DECIMAL",
+                "pmnonvolatile_number": "DECIMAL",
+                "source_wkt": "TEXT",
+            },
+            primary_key="timestamp",
+            # TODO OPENGIS.ch: add the geometry column
+            # geometry_columns=[
+            #     {
+            #         "column_name": "source_geometry",
+            #         "SRID": 3857,
+            #         "geometry_type": "POLYGON",
+            #         "geometry_type_dimension": 2,
+            #     },
+            # ],
+        )
+        serializer._recreate_table(filename)
 
-    def __init__(
-        self,
-        db_path_string,
-        table_name_string="emission_calculation_result",
-        table_columns_type_dict=None,
-        primary_key="time",
-        geometry_columns=None,
-    ):
+        insert_into_table(filename, table_name, self.rows)
 
-        if table_columns_type_dict is None:
-            table_columns_type_dict = OrderedDict(
-                [
-                    ("time", "DATETIME"),
-                    ("source_id", "TEXT"),
-                    ("co_kg", "DECIMAL"),
-                    ("co2_kg", "DECIMAL"),
-                    ("hc_kg", "DECIMAL"),
-                    ("nox_kg", "DECIMAL"),
-                    ("sox_kg", "DECIMAL"),
-                    ("pmtotal_kg", "DECIMAL"),
-                    ("pm01_kg", "DECIMAL"),
-                    ("pm25_kg", "DECIMAL"),
-                    ("pmsul_kg", "DECIMAL"),
-                    ("pmvolatile_kg", "DECIMAL"),
-                    ("pmnonvolatile_kg", "DECIMAL"),
-                    ("pmnonvolatile_number", "DECIMAL"),
-                ]
+        if os.path.isfile(filename):
+            QtWidgets.QMessageBox.information(
+                None,
+                "SQLiteOutputModule",
+                f"Results saved as SQLite file at `{filename}`",
             )
-        if geometry_columns is None:
-            geometry_columns = []
-
-        SQLSerializable.__init__(
-            self,
-            db_path_string,
-            table_name_string,
-            table_columns_type_dict,
-            primary_key,
-            geometry_columns,
-        )
