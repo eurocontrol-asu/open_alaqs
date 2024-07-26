e8a810c4f55f5f018235a46188f967b11ea3c750
Luigi Pirelli <luipir@gmail.com>
2024-07-26 18:15:27 +0200

    followout of 742fb195c48e3499f393dcbdd5b63a50f010df19

--- open_alaqs/ui/ui_roadways.py
+++ open_alaqs/ui/ui_roadways.py
@@ -29,12 +29,10 @@ def catch_errors(f):
 
 
 def form_open(form, layer, feature):
-    # avoid trigger error becasue called under widget are availales yet
-    # This form have to be used when creating a new feature of showing the form in attribute table.
-    # The follwoing lines avoid trigger error when opening form from editing tools because the
-    # dialog is not yet visible and can deal to Non values when findChild
-    if not form.isVisible():
-        return
+    logger.debug("This is the modified simple form")
+    logger.debug(f"Layer {layer} and feature {feature}")
+    logger.debug(f"Attributes of fields: {feature.fields().names()}")
+    logger.debug(f"Attributes of feature: {feature.attributes()}")
 
     # avoid trigger error becasue called under widget are availales yet
     # This form have to be used when creating a new feature of showing the form in attribute table.
