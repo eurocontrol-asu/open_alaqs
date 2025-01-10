932a2791f45c01e8d7eeca20ccc03be1bfb69240
Stavros <156215090+sstromat@users.noreply.github.com>
2025-01-10 10:53:38 +0100

    Update Movement.py

--- open_alaqs/core/interfaces/Movement.py
+++ open_alaqs/core/interfaces/Movement.py
@@ -1673,13 +1673,13 @@ class Movement:
         runway_end_point = QgsPointXY(runway_points[-1])
         runway_directions = self.getRunway().getDirections()
         rot_angle = (
-            180 if self.getTrajectory().getDepartureArrivalFlag()=="D" else 360
+            180 if self.getTrajectory().getDepartureArrivalFlag() == "D" else 360
         )
 
         if self.getRunwayDirection() == runway_directions[1]:
             runway_backup_point = runway_start_point
             runway_azimuth_deg = (
-                math.degrees(d.bearing(runway_start_point, runway_end_point)) 
+                math.degrees(d.bearing(runway_start_point, runway_end_point))
                 + rot_angle
             ) % 360
         elif self.getRunwayDirection() == runway_directions[0]:
